8a18d20730dc381aa7709132e70d123a45d3c5af
MHS <hayaruby7909@gmail.com>
2025-11-28 14:50:01 +0900

    feat: mollang 추가

--- src/main/java/kr/soft/git/MemberContoller.java
+++ src/main/java/kr/soft/git/MemberContoller.java
@@ -1,9 +1,16 @@
 package kr.soft.git;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MemberContoller {
 
+    @GetMapping("/mollang")
+    public void mollang() {
+      log.info("mollang");
+    }
 
 }
