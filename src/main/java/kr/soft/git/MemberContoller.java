74d433d47b210e87624f80fc2f8f6e403b1ff101
unknown <dongoukm8@gmail.com>
2025-11-28 14:47:52 +0900

    babo

--- src/main/java/kr/soft/git/MemberContoller.java
+++ src/main/java/kr/soft/git/MemberContoller.java
@@ -1,16 +1,18 @@
 package kr.soft.git;
 
 import ch.qos.logback.classic.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+@Slf4j
 @RestController
 public class MemberContoller {
-
-    @GetMapping ("/mollang")
-    public void mollang(){
-        log.info("mollang");
+    @GetMapping("/babo")
+    public void babo(){
+        log.info("babo");
     }
-
-
 }
+
+
