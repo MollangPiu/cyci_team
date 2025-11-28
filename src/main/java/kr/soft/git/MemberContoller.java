6313ba1ccd30bdd3ef6b894fa06f3b143385d2f7
unknown <dongoukm8@gmail.com>
2025-11-28 14:39:10 +0900

    A

--- src/main/java/kr/soft/git/MemberContoller.java
+++ src/main/java/kr/soft/git/MemberContoller.java
@@ -1,9 +1,16 @@
 package kr.soft.git;
 
+import ch.qos.logback.classic.Logger;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberContoller {
+
+    @GetMapping ("/mollang")
+    public void mollang(){
+        log.info("mollang");
+    }
 
 
 }
